b0f405043a972c4c1d9a19ecc82bae6a6a7f1495
samirvithlani <samir.vithlani83955@gmail.com>
2025-12-06 11:29:50 +0530

    delete

--- src/api/ApiDemo1.jsx
+++ src/api/ApiDemo1.jsx
@@ -1,12 +1,16 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import { Loader } from '../components/Loader'
 
 export const ApiDemo1 = () => {
+    //DELETE//https://node5.onrender.com/user/user/salnshashsau98y3u892us
 
     const [message, setmessage] = useState("")
     const [users, setusers] = useState([])
+    const [isLoading, setisLoading] = useState(false)
     const getApiCall =async()=>{
         //Promise<AxiosResponse<any, any, {}>>
+        setisLoading(true)
         const res = await axios.get("https://node5.onrender.com/user/user")
         console.log(res) //axios object
         console.log(res.data) //api response...
@@ -14,20 +18,57 @@ export const ApiDemo1 = () => {
         console.log(res.data.data)
         setmessage(res.data.message)
         setusers(res.data.data)
+        setisLoading(false)
+    }
+
+    const deleteUser = async(id)=>{
+        const res = await axios.delete(`https://node5.onrender.com/user/user/${id}`)
+        console.log(res)
+        if(res.status == 204){
+            alert("user deleted...")
+            //getAPI CALL...database updated..
+            getApiCall()
+        }
     }
 
   return (
     <div style={{textAlign:"center"}}>
+        {
+            isLoading && <Loader/>
+        }
         <button onClick={()=>{getApiCall()}}>GET</button>
         <h1>API DEMO 1</h1>
         {message}
-        <ul>
-            {
-                users.map((user)=>{
-                    return<li>{user.name}</li>
-                })
-            }
-        </ul>
+        <table className='table table-dark'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NAME</th>
+                    <th>AGE</th>
+                    <th>EMAIL</th>
+                    <th>STATUS</th>
+                    <th>ACTION</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    users.map((user)=>{
+                        return<tr>
+                            <td
+                            >{user._id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.age}</td>
+                            <td>{user.email}</td>
+                            <td>{user.isActive ?"True":"False"}</td>
+                            <td>
+                                <button onClick={()=>{deleteUser(user._id)}} className='btn btn-danger'>DELETE</button>
+                            </td>
+                        </tr>
+                    })
+                }
+            </tbody>
+        </table>
     </div>
   )
 }
---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../assets/css/loader.css"
+
+export const Loader = () => {
+  return (
+    <div class="loader"></div>
+  )
+}
