d6b7aabf07670fe65dcbd0097054eb84f5e32573
samirvithlani <samir.vithlani83955@gmail.com>
2025-12-06 12:32:05 +0530

    detail

--- src/api/ApiDemo1.jsx
+++ src/api/ApiDemo1.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Loader } from '../components/Loader'
+import { Button, Modal } from 'react-bootstrap'
 
 export const ApiDemo1 = () => {
     //DELETE//https://node5.onrender.com/user/user/salnshashsau98y3u892us
@@ -8,6 +9,11 @@ export const ApiDemo1 = () => {
     const [message, setmessage] = useState("")
     const [users, setusers] = useState([])
     const [isLoading, setisLoading] = useState(false)
+    const [singleUser, setsingleUser] = useState({})
+    const [show, setshow] = useState(false)
+
+    const handleClose = ()=>setshow(false)
+    
     const getApiCall =async()=>{
         //Promise<AxiosResponse<any, any, {}>>
         setisLoading(true)
@@ -30,6 +36,15 @@ export const ApiDemo1 = () => {
             getApiCall()
         }
     }
+
+    const getUserDetail = async(id)=>{
+        const res = await axios.get(`https://node5.onrender.com/user/user/${id}`)
+        console.log(res.data.data) //->browser --useState
+        setsingleUser(res.data.data)
+        //open model..
+        setshow(true)
+    }
+
     useEffect(()=>{
         getApiCall()
     },[])
@@ -66,12 +81,28 @@ export const ApiDemo1 = () => {
                             <td>{user.isActive ?"True":"False"}</td>
                             <td>
                                 <button onClick={()=>{deleteUser(user._id)}} className='btn btn-danger'>DELETE</button>
+                                <button onClick={()=>{getUserDetail(user._id)}}  className='btn btn-info'>INFO</button>
                             </td>
                         </tr>
                     })
                 }
             </tbody>
         </table>
+         <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>USER DETAIL</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this user detail!
+            <p>{singleUser.name}</p>
+            <p>{singleUser.email}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
